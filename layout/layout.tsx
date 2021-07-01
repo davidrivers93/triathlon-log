2cc52b99e97b6599f783cfe0cace60ce18df32a8
David <davidriosbenet@gmail.com>
2021-07-01 19:06:17 +0200

    feat: Add layout component

---
+++ layout/layout.tsx
@@ -0,0 +1,26 @@
+import React, { PropsWithChildren } from "react";
+import Navbar from "./navbar";
+
+const Layout = ({ children }: PropsWithChildren<any>) => {
+  return (
+    <>
+      <Navbar />
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Triathlons</h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border border-gray-200 rounded-lg h-96">
+              {children}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Layout;
