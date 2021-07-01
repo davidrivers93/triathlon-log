93d19e7297c38cdc265952c96cf8e1ccca760117
David <davidriosbenet@gmail.com>
2021-07-01 18:37:20 +0200

    feat: integrate heroku

---
+++ models/triathlon.ts
@@ -0,0 +1,4 @@
+export interface Triathlon {
+  name: string;
+  id: string;
+}
--- pages/triathlons/[slug].tsx
+++ pages/triathlons/[slug].tsx
@@ -21,17 +21,24 @@ const Triathlons = (props: { entriesData: Triathlon[] }) => {
   );
 };
 
-export const getStaticProps = async () => {
-  const entries = await db.collection(DATABASE_CONST.TRIATHLONS).get();
-  const entriesData = entries.docs.map((entry) => ({
-    id: entry.id,
-    ...entry.data(),
-  })) as Triathlon[];
-
-  return {
-    props: { entriesData },
-    revalidate: 10,
-  };
+export const getStaticProps = async (context) => {
+  const { id } = context.params;
+  const res = await db
+    .collection(DATABASE_CONST.TRIATHLONS)
+    .where("id", "==", id)
+    .get();
+  const entry = res.docs.map((entry) => entry.data());
+  if (entry.length) {
+    return {
+      props: {
+        entry: entry[0],
+      },
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
 };
 
 export default Triathlons;
